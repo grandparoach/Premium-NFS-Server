923380a3a7684d760bed94078029184da7a122b5
Steven Roach <stevenwroach@hotmail.com>
2018-06-26 13:19:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,36 @@
+#!/bin/bash
 
-[[ -z "$HOME" || ! -d "$HOME" ]] && { echo 'fixing $HOME'; HOME=/root; } 
-export HOME 
-
-# Set user args
-MASTER_HOSTNAME=$1
+set -x
 
 # Shares
-SHARE_HOME=/share/home
-SHARE_DATA=/share/data
+NFS_DATA=/share/data
+mkdir -p $NFS_DATA
 
-# Hpc User
-HPC_USER=$2
-HPC_UID=7007
-HPC_GROUP=hpc
-HPC_GID=7007
+BLACKLIST="/dev/sda|/dev/sdb"
 
+scan_for_new_disks() {
+    # Looks for unpartitioned disks
+    declare -a RET
+    DEVS=($(ls -1 /dev/sd*|egrep -v "${BLACKLIST}"|egrep -v "[0-9]$"))
+    for DEV in "${DEVS[@]}";
+    do
+        # Check each device if there is a "1" partition.  If not,
+        # "assume" it is not partitioned.
+        if [ ! -b ${DEV}1 ];
+        then
+            RET+="${DEV} "
+        fi
+    done
+    echo "${RET}"
+}
 
-# Returns 0 if this node is the master node.
-#
-is_master()
-{
-    hostname | grep "$MASTER_HOSTNAME"
-    return $?
+get_disk_count() {
+    DISKCOUNT=0
+    for DISK in "${DISKS[@]}";
+    do 
+        DISKCOUNT+=1
+    done;
+    echo "$DISKCOUNT"
 }
 
 
@@ -29,142 +38,53 @@ is_master()
 #
 install_pkgs()
 {
-    pkgs="zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget"
-    yum -y install $pkgs
-}
-
-# Partitions all data disks attached to the VM and creates
-# a RAID-0 volume with them.
-#
-setup_data_disks()
-{
-    mountPoint="$1"
-    createdPartitions=""
-
-    # Loop through and partition disks until not found
-    for disk in sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm sdn sdo sdp sdq sdr; do
-        fdisk -l /dev/$disk || break
-        fdisk /dev/$disk << EOF
-n
-p
-1
-
-
-t
-fd
-w
-EOF
-        createdPartitions="$createdPartitions /dev/${disk}1"
-    done
-
-    # Create RAID-0 volume
-    if [ -n "$createdPartitions" ]; then
-        devices=`echo $createdPartitions | wc -w`
-        mdadm --create /dev/md10 --level 0 --raid-devices $devices $createdPartitions
-        mkfs -t ext4 /dev/md10
-        echo "/dev/md10 $mountPoint ext4 defaults,nofail 0 2" >> /etc/fstab
-        mount /dev/md10
-    fi
-}
-
-# Creates and exports two shares on the master nodes:
-#
-# /share/home (for HPC user)
-# /share/data
-#
-# These shares are mounted on all worker nodes.
-#
-setup_shares()
-{
-    mkdir -p $SHARE_HOME
-    mkdir -p $SHARE_DATA
-
-    if is_master; then
-        setup_data_disks $SHARE_DATA
-        echo "$SHARE_HOME    *(rw,async)" >> /etc/exports
-        echo "$SHARE_DATA    *(rw,async)" >> /etc/exports
-
-        # systemctl enable rpcbind || echo "Already enabled"
-        # systemctl enable nfs-server || echo "Already enabled"
-        # systemctl start rpcbind || echo "Already enabled"
-        # systemctl start nfs-server || echo "Already enabled"
-        service nfs start
-    else
-        echo "master:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
-        echo "master:$SHARE_DATA $SHARE_DATA    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
-        mount -a
-        mount | grep "^master:$SHARE_HOME"
-        mount | grep "^master:$SHARE_DATA"
-    fi
+    yum -y install epel-release
+    yum -y install nfs-utils nfs-utils-lib rpcbind mdadm 
 }
 
 
-# Adds a common HPC user to the node and configures public key SSh auth.
-# The HPC user has a shared home directory (NFS share on master) and access
-# to the data share.
-#
-setup_hpc_user()
+setup_raid()
 {
-    # disable selinux
-    sed -i 's/enforcing/disabled/g' /etc/selinux/config
-    setenforce permissive
+	#Verify attached data disks
+	ls -l /dev | grep sd
+	
+	DISKS=($(scan_for_new_disks))
+    echo "Disks are ${DISKS[@]}"
+    declare -i DISKCOUNT
+    DISKCOUNT=$(get_disk_count) 
+    echo "Disk count is $DISKCOUNT"
+	
+	#Create RAID md device
+	mdadm -C /dev/md0 -l raid0 -n "$DISKCOUNT" "${DISKS[@]}"
+
+    #Create File System
+    mkfs -t xfs /dev/md0
+    echo "/dev/md0 $NFS_DATA xfs rw,noatime,attr2,inode64,nobarrier,sunit=1024,swidth=4096,nofail 0 2" >> /etc/fstab
+}
+
+mount_nfs()
+{
+
+    echo "$NFS_DATA    *(rw,async)" >> /etc/exports
+    systemctl enable rpcbind || echo "Already enabled"
+    systemctl enable nfs-server || echo "Already enabled"
+    systemctl start rpcbind || echo "Already enabled"
+    systemctl start nfs-server || echo "Already enabled"
     
-    groupadd -g $HPC_GID $HPC_GROUP
-
-    # Don't require password for HPC user sudo
-    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    
-    # Disable tty requirement for sudo
-    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
-
-    if is_master; then
-    
-        useradd -c "HPC User" -g $HPC_GROUP -m -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
-
-        mkdir -p $SHARE_HOME/$HPC_USER/.ssh
-        
-        # Configure public key auth for the HPC user
-        ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
-        cat $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub > $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
-
-        echo "Host *" > $SHARE_HOME/$HPC_USER/.ssh/config
-        echo "    StrictHostKeyChecking no" >> $SHARE_HOME/$HPC_USER/.ssh/config
-        echo "    UserKnownHostsFile /dev/null" >> $SHARE_HOME/$HPC_USER/.ssh/config
-        echo "    PasswordAuthentication no" >> $SHARE_HOME/$HPC_USER/.ssh/config
-
-        # Fix .ssh folder ownership
-        chown -R $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER
-
-        # Fix permissions
-        chmod 700 $SHARE_HOME/$HPC_USER/.ssh
-        chmod 644 $SHARE_HOME/$HPC_USER/.ssh/config
-        chmod 644 $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
-        chmod 600 $SHARE_HOME/$HPC_USER/.ssh/id_rsa
-        chmod 644 $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub
-        
-        # Give hpc user access to data share
-        chown $HPC_USER:$HPC_GROUP $SHARE_DATA
-    else
-        useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
-    fi
+    exportfs
+	exportfs -a
+	exportfs 
 }
 
-# Sets all common environment variables and system parameters.
-#
-setup_env()
-{
-    # Set unlimited mem lock
-    echo "$HPC_USER hard memlock unlimited" >> /etc/security/limits.conf
-    echo "$HPC_USER soft memlock unlimited" >> /etc/security/limits.conf
+systemctl stop firewalld
+systemctl disable firewalld
 
-    # Intel MPI config for IB
-    echo "# IB Config for MPI" > /etc/profile.d/hpc.sh
-    echo "export I_MPI_FABRICS=shm:dapl" >> /etc/profile.d/hpc.sh
-    echo "export I_MPI_DAPL_PROVIDER=ofa-v2-ib0" >> /etc/profile.d/hpc.sh
-    echo "export I_MPI_DYNAMIC_CONNECTION=0" >> /etc/profile.d/hpc.sh
-}
+# Disable SELinux
+sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
 
 install_pkgs
-setup_shares
-setup_hpc_user
-setup_env
+setup_raid
+mount_nfs
+
+exit 0
