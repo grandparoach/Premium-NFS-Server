c2b749e2de73b1e26defa64350bcd34564202547
Steven Roach <stevenwroach@hotmail.com>
2018-07-09 10:45:58 -0700

    Update client-setup.sh

--- client-setup.sh
+++ client-setup.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-set -x
-
-MASTER_NAME=$1
-echo $MASTER_NAME
-
-# Shares
-NFS_DATA=/share/data
-mkdir -p $NFS_DATA
-
 install_pkgs()
 {
     yum -y install epel-release
-    yum -y install nfs-utils nfs-utils-lib rpcbind  
-}
-
-
-mount_nfs()
-{
-	showmount -e ${MASTER_NAME}
-	mount -t nfs ${MASTER_NAME}:${NFS_DATA} ${NFS_DATA}
-	
-	echo "${MASTER_NAME}:${NFS_DATA} ${NFS_DATA} nfs defaults,nofail  0 0" >> /etc/fstab
 }
 
 
@@ -33,6 +14,5 @@ sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
 
 install_pkgs
-mount_nfs
 
 exit 0
