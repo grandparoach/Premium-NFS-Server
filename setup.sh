a8a3667b96f99b903e6621c8fffef9286b4d3157
Steven Roach <stevenwroach@hotmail.com>
2019-02-27 11:19:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,52 +7,26 @@ NFS_DATA=/share/data
 mkdir -p $NFS_DATA
 chmod 777 $NFS_DATA
 
-BLACKLIST="/dev/sda|/dev/sdb"
-
-scan_for_new_disks() {
-    # Looks for unpartitioned disks
-    declare -a RET
-    DEVS=($(ls -1 /dev/sd*|egrep -v "${BLACKLIST}"|egrep -v "[0-9]$"))
-    for DEV in "${DEVS[@]}";
-    do
-        # Check each device if there is a "1" partition.  If not,
-        # "assume" it is not partitioned.
-        if [ ! -b ${DEV}1 ];
-        then
-            RET+="${DEV} "
-        fi
-    done
-    echo "${RET}"
-}
-
-get_disk_count() {
-    DISKCOUNT=0
-    for DISK in "${DISKS[@]}";
-    do 
-        DISKCOUNT+=1
-    done;
-    echo "$DISKCOUNT"
-}
 
 
 # Installs all required packages.
 #
 install_pkgs()
 {
-    yum -y install epel-release
-    yum -y install nfs-utils nfs-utils-lib rpcbind mdadm 
+    apt install -y nfs-server 
 }
 
 
 setup_raid()
 {
 	#Verify attached data disks
-	ls -l /dev | grep sd
-	
-	DISKS=($(scan_for_new_disks))
+	ls -l /dev | grep nvme
+
+	DISKS=($(ls /dev/nvme*n1))
     echo "Disks are ${DISKS[@]}"
+
     declare -i DISKCOUNT
-    DISKCOUNT=$(get_disk_count) 
+    DISKCOUNT=($(ls -1 /dev/nvme*n1 | wc -l))
     echo "Disk count is $DISKCOUNT"
 	
 	#Create RAID md device
@@ -61,12 +35,18 @@ setup_raid()
     #Create File System
     mkfs -t xfs /dev/md0
     echo "/dev/md0 $NFS_DATA xfs rw,noatime,attr2,inode64,nobarrier,sunit=1024,swidth=4096,nofail 0 2" >> /etc/fstab
+
+    mount -a
+    
 }
 
 mount_nfs()
 {
+    #increase the NFS threads to 80
+    sed -i 's/RPCNFSDCOUNT=8/RPCNFSDCOUNT=80/' /etc/init.d/nfs-kernel-server
+    sed -i 's/RPCNFSDCOUNT=8/RPCNFSDCOUNT=80/' /etc/default/nfs-kernel-server
 
-    echo "$NFS_DATA    *(rw,async)" >> /etc/exports
+    echo "$NFS_DATA    *(rw,async,no_root_squash)" >> /etc/exports
     systemctl enable rpcbind || echo "Already enabled"
     systemctl enable nfs-server || echo "Already enabled"
     systemctl start rpcbind || echo "Already enabled"
@@ -77,8 +57,8 @@ mount_nfs()
 	exportfs 
 }
 
-systemctl stop firewalld
-systemctl disable firewalld
+#systemctl stop firewalld
+#systemctl disable firewalld
 
 # Disable SELinux
 sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
